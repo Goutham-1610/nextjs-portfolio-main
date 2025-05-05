792a935305256b0b4ab73b03dc0cffba07a03a19
goutham <155947166+Goutham-1610@users.noreply.github.com>
2025-05-05 08:30:25 +0530

    first change

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-red-400">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-100 to-yellow-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>

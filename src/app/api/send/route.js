00048aed90233dbfdf28ac11aa3dc9f3cdbea054
goutham <155947166+Goutham-1610@users.noreply.github.com>
2025-05-05 08:33:53 +0530

    waiting for domain

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,34 +1,34 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import React from "react"; // Needed for JSX in Resend
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import React from "react"; // Needed for JSX in Resend
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req) {
-  const { email, subject, message } = await req.json();
-  console.log("API KEY:", process.env.RESEND_API_KEY); // Debug
-  console.log(email, subject, message);
+// export async function POST(req) {
+//   const { email, subject, message } = await req.json();
+//   console.log("API KEY:", process.env.RESEND_API_KEY); // Debug
+//   console.log(email, subject, message);
 
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: "hello",
-      react: (
-        <>
-        <p>email body</p>
-          {/* <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p> */}
-        </>
-      ),
-    });
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: [fromEmail, email],
+//       subject: "hello",
+//       react: (
+//         <>
+//         <p>email body</p>
+//           {/* <h1>{subject}</h1>
+//           <p>Thank you for contacting us!</p>
+//           <p>New message submitted:</p>
+//           <p>{message}</p> */}
+//         </>
+//       ),
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error("Send error:", error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     console.error("Send error:", error);
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
